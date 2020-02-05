d9d9646d919adc25b78cb2a15cba9a7dfa45199f
Michael Lim <michaellim@Michaels-MacBook-Pro.local>
2020-02-05 15:03:16 -0500

    remove files from cs2204 project

--- js/pushhead.js
+++
@@ -1,8 +0,0 @@
-var hdg=[];
-window.addEventListener("load",pushHeading);
-function pushHeading() {
-  var thd = document.getElementById("cart").querySelector("thead").querySelectorAll("th");
-  for(var i=0;i<thd.length;i++) {
-    hdg.push(thd[i].id);
-  }
-}
